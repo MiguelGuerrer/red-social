f00d77657d69c13a661f61a97c968250b140f2e7
MiguelGuerrer <guerrerom@udesa.edu.ar>
2022-11-03 18:59:06 -0300

    mysql

--- db.sql
+++ db.sql
@@ -36,6 +36,16 @@ createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
 updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 );
 
+CREATE TABLE seguidores(
+id int unsigned primary key auto_increment,
+id_seguido int unsigned,
+id_seguidor int unsigned,
+foreign key (id_seguido) references usuarios(id),
+foreign key (id_seguidor) references usuarios(id),
+createdAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+updatedAt TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+);
+
 /*USUARIOS*/
 INSERT INTO  usuarios (`id`, `email`, `contrasenia`, `foto`, `fecha`, `DNI`) values (default,'michaelscott@gmail.com','scott1234','/public/images/scott.jpg','1988-06-22',34683203);
 INSERT INTO  usuarios (`id`, `email`, `contrasenia`, `foto`, `fecha`, `DNI`) values (default,'catadiaz@gmail.com','diaz1234','/public/images/diaz.jpg','1978-06-12',89753203);
@@ -44,24 +54,24 @@ INSERT INTO  usuarios (`id`, `email`, `contrasenia`, `foto`, `fecha`, `DNI`) val
 INSERT INTO  usuarios (`id`, `email`, `contrasenia`, `foto`, `fecha`, `DNI`) values (default,'dariobenedetto@gmail.com','dario1234','/public/images/benedetto.jpg','1972-09-02',78353203);
 
 /*POSTEOS*/
-INSERT INTO posteos(id, id_usuarios, image_name, pie_post)
-values(default, 1 , '/img/usuarios/mc.png', 'Hola');
-INSERT INTO posteos values (default,2,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,3,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,4,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,5,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,1,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,2,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,3,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,4,'/img/michael/mc.png','michael');
-INSERT INTO posteos values (default,5,'/img/michael/mc.png','michael');
+INSERT INTO posteos(id, id_usuario, nombre_imagen, texto_imagen)
+values(default, 1 , '/img/Michael/mc.png', 'Michael');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,2,'/img/catadiaz/mc.png','cata diaz');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,3,'/img/guerrero/mc.png','guerrero');
+INSERT INTO posteos  (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,4,'/img/wos/mc.png','wos');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,5,'/img/dariobenedetto/mc.png','benedetto');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,1,'/img//catadiaz.png','diaz');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,2,'/img/guerrero/mc.png','miguelguerrero');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,3,'/img/wos/mc.png','wos');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,4,'/img/michaelscott/mc.png','michael');
+INSERT INTO posteos (`id`, `id_usuario`, `nombre_imagen`, `texto_imagen`) values (default,5,'/img/benedetto/mc.png','dariobenedetto');
 
 
 /*COMENTARIOS*/
-INSERT INTO comentario values (default, 1, 1, 'Me gusta tu foto');
-INSERT INTO comentario values (default, 1, 1, 'Me gusta tu foto');
-INSERT INTO comentario values (default, 1, 1, 'Me gusta tu foto');
-INSERT INTO comentario values (default, 1, 1, 'Me gusta tu foto');
+INSERT INTO comentarios (`id`,`id_posteo`,`id_usuario`,`texto_comentario`) values (default, 2, 1, 'Me gusta tu foto');
+INSERT INTO comentarios (`id`,`id_posteo`,`id_usuario`,`texto_comentario`) values (default, 2, 2, 'hola');
+INSERT INTO comentarios (`id`,`id_posteo`,`id_usuario`,`texto_comentario`) values (default, 3, 3, 'buena foto');
+INSERT INTO comentarios (`id`,`id_posteo`,`id_usuario`,`texto_comentario`) values (default, 4, 4, 'cuando nos vemos');
 
 
 
